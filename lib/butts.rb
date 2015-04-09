1be7a6b8940f6211ba23516661aa7e24d0fe01eb
William Mathewson <wncmathewson@me.com>
2015-04-09 15:12:21 +0100

    Made variables available across the module

--- lib/butts.rb
+++ lib/butts.rb
@@ -2,14 +2,14 @@ require 'httparty'
 require_relative 'butts/version'
 
 module Butts
-  butts_uri = 'butts.so/api/v1'
-  header = { 'Content-Type' => 'application/json' }
+  @butts_uri = 'http://butts.so/api/v1'
+  @header = { 'Content-Type' => 'application/json' }
 
   def self.get_short_url(url)
     options = { body: {url: url}.to_json }
-    options[:headers] = header
+    options[:headers] = @header
 
-    shortcode = HTTParty.post("#{butts_uri}/create", options)
+    shortcode = HTTParty.post("#{@butts_uri}/create", options)
 
     "http://butts.so/#{shortcode['shortcode']}"
   end
